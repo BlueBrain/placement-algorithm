12f28c07b26b3eddf26f02cf2a98a9a6f0b3c3bb
Anirudh Vij <anirudh.vij@epfl.ch>
2011-08-23 17:58:04 +0000

    adding function to return version of tool
    git-svn-id: https://bbp.epfl.ch/svn/placementAlgorithm/trunk@48 85ab80b9-0834-0410-868f-c84dfc379727

---
+++ Placement/PlacementHints/getVersion.m
@@ -0,0 +1,7 @@
+%%Copyright © BBP/EPFL 2005-2011; All rights reserved. Do not distribute without further notice
+% Simple Version function
+% Last Edit by $Author: vij $
+% Last Update $Rev: 47 $
+
+function VersionString= getVersion ()
+VersionString='September 2011,R1';
