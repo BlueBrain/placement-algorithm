6b6eb48c762df410d73e737da29ea6e61078130d
Michael Reimann <michael.reimann@epfl.ch>
2012-02-08 14:55:11 +0000

    Those 2 functions were not needed after all. Made it all dependent on parseXML instead. That file is available in BlueTools
    git-svn-id: https://bbp.epfl.ch/svn/placementAlgorithm/branches/xmlRules@61 85ab80b9-0834-0410-868f-c84dfc379727

--- Placement/PlacementHints/getLayerDefinition.m
+++ Placement/PlacementHints/getLayerDefinition.m
@@ -2,14 +2,29 @@
 function Layer = getLayerDefinition(recipe)
 % define Layer Boundaries
 
-xml = xml_read(recipe);
+ifElseExpansion = @(x,ie)ie{double(x)+1};            
+nameFieldLookup = @(x,str,fn)ifElseExpansion(any(cellfun(@(s)strcmp(s,str),{x.Name})),{'',x(find(cellfun(@(s)strcmp(s,str),{x.Name}),1,'last')).(fn)});
 
-layers = xml.column.layer;
-maxLayer = length(layers);
+if(exist('parseXML','file'))
+    xml = parseXML(recipe);
+else
+    xml = xml2struct(recipe);
+end
+
+column = nameFieldLookup(xml,'blueColumn','Children');
+column = nameFieldLookup(column,'column','Children');
+
+layers = column(cellfun(@(x)strcmp(x,'layer'),{column.Name}));
+for i = 1:length(layers)
+    layerNum = str2double(nameFieldLookup(layers(i).Attributes,'id','Value'));
+    thickness(layerNum) = str2double(nameFieldLookup(layers(i).Attributes,'thickness','Value'));
+end
+
+maxLayer = length(thickness);
 currMin = 0;
 
 for i = maxLayer:-1:1
     Layer(i).From = currMin;
-    Layer(i).To = currMin + layers(i).ATTRIBUTE.thickness;
+    Layer(i).To = currMin + thickness(i);
     currMin = Layer(i).To;
 end
