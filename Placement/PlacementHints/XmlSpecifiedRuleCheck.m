c6fd3e428d1ab4f1864684401aa2d270cb15f3ee
Michael Reimann <michael.reimann@epfl.ch>
2012-02-08 10:40:36 +0000

    Added a new rule type: y_below
    git-svn-id: https://bbp.epfl.ch/svn/placementAlgorithm/branches/xmlRules@57 85ab80b9-0834-0410-868f-c84dfc379727

--- Placement/PlacementHints/XmlSpecifiedRuleCheck.m
+++ Placement/PlacementHints/XmlSpecifiedRuleCheck.m
@@ -14,6 +14,7 @@ classdef XmlSpecifiedRuleCheck < handle
         yBinsSize; %TODO: offer set function
         layerBins;
         transferFcn;
+        ruleCheckFcn;
     end
     properties(Access=public)
         strictness;
@@ -23,10 +24,11 @@ classdef XmlSpecifiedRuleCheck < handle
         function obj = XmlSpecifiedRuleCheck(file,lInfo)
             obj.ruleSetFile = file;
             obj.layerInfo = lInfo;
-            obj.yBinsSize = 10; %micron
-            obj.transferFcn.id = @(x)x;
-            obj.transferFcn.default = @(x)x.^2;
+            obj.yBinsSize = 10; %micron            
+            obj.transferFcn.default = @(x)x;
             obj.transferFcn.upper_hard_limit = @(x)obj.upper_hard_limit(x);
+            obj.ruleCheckFcn.region_target = @(instance,rule,bins)obj.checkRegionTarget(instance,rule,bins);
+            obj.ruleCheckFcn.y_below = @(instance,rule,bins)obj.checkYBelow(instance,rule,bins);
             obj.makeLayerBins();
             %then do parsing here            
             xml = parseXML(file);
@@ -56,13 +58,14 @@ classdef XmlSpecifiedRuleCheck < handle
             annotations = annotations(cellfun(@(x)strcmp(x,'placement'),{annotations.Name}));
             for i = 1:length(annotations)
                 instance = nameFieldLookup(annotations(i).Children,'use_rule','Attributes');
+                
                 ruleName = nameFieldLookup(instance,'rule','Value');
-                %Right now we only consider y intervals. Any other cases
-                %planned?
-                y_min = str2double(nameFieldLookup(instance,'y_min','Value'));
-                y_max = str2double(nameFieldLookup(instance,'y_max','Value'));
                 obj.morphologyRuleInstances.(morphName)(i).Rule = ruleName;
-                obj.morphologyRuleInstances.(morphName)(i).Interval = [y_min y_max];
+                
+                otherAttributes = setdiff({instance.Name},'rule');
+                for j = 1:length(otherAttributes)
+                    obj.morphologyRuleInstances.(morphName)(i).(otherAttributes{j})=nameFieldLookup(instance,otherAttributes{j},'Value');
+                end                
             end            
         end
         function scores = getResult(obj,morphName,inLayer,asMType)            
@@ -124,46 +127,50 @@ classdef XmlSpecifiedRuleCheck < handle
                 relevantRuleSet = obj.ruleSets.(asMType); 
                 scores = ones(0,length(obj.layerBins{inLayer}));
                 for i = 1:length(relevantInstances)
-                    scores(i,:) = obj.checkRuleInstance(relevantInstances(i),relevantRuleSet,obj.layerBins{inLayer});
+                    if(~isfield(relevantRuleSet,relevantInstances(i).Rule))
+                        if(isfield(obj.ruleSets.global,relevantInstances(i).Rule))
+                            relevantRule = obj.ruleSets.global.(relevantInstances(i).Rule);
+                        else
+                            error('PlacementHints:getResults:UnknownRule','Rule instance refers to rule %s in %s, which is not defined! Neither defined as global',...
+                            relevantInstances(i).Rule,relevantRuleSet.mtype);
+                        end
+                    else
+                        relevantRule = relevantRuleSet.(relevantInstances(i).Rule);
+                    end                    
+                    if(isfield(relevantInstances(i),'transferFcn'))
+                        tferFunc = obj.transferFcn.(strrep(relevantInstances(i).transferFcn,'-','_'));
+                    else
+                        tferFunc = obj.transferFcn.default;
+                    end
+                    scores(i,:) = tferFunc(obj.ruleCheckFcn.(relevantRule.type)(relevantInstances(i),relevantRule,obj.layerBins{inLayer}));
                 end
                 scores = mergeScores(scores);
             end
         end
-        function scores = checkRuleInstance(obj,instance,set,bins)
-            if(~isfield(set,instance.Rule))
-                error('PlacementHints:getResults:UnknownRule','Rule instance refers to rule %s in %s, which is not defined!',...
-                    instance.Rule,set.mtype);
-            end
-            minMaxFcn = @(x,y)cat(2,max(x(1),y(1)),min(x(2),y(2)));
-            intervalAbs = obj.getLayerInterval(set.(instance.Rule));            
-            scores = cellfun(@(x)diff(minMaxFcn(intervalAbs,instance.Interval+x)),num2cell(bins))./min(diff(intervalAbs),diff(instance.Interval));
-            scores(scores<0)=0;
-            if(isfield(instance,'transferFcn'))
-                scores = obj.transferFcn.(strrep(instance.transferFcn,'-','_'))(scores);
-            else
-                scores = obj.transferFcn.default(scores);
-            end
-        end
+        
         function scores = mergeScores(obj,scores)  
             if(isempty(scores))
                 scores = ones(1,size(scores,2));
                 return;
             end
-            fcn = @(x,p)((sum(x.^p)/length(x))^(1/p));
+            fcn = @(x,p)((sum(x.^p)/length(x))^(1/p))*all(x>=0);
             shapeParameter = norminv(obj.strictness/101,1,2.25);            
-            baseWeight = size(scores,1).*(log10(obj.strictness).^10);
-            scores = round(baseWeight.*cellfun(@(x)fcn(x,shapeParameter),num2cell(scores,1))+ones(1,size(scores,2)));
+            baseWeight = size(scores,1).*(log10(obj.strictness).^7);
+            scores = round(baseWeight.*cellfun(@(x)fcn(x,shapeParameter),num2cell(scores,1)));%+ones(1,size(scores,2)));
         end
         
         %Function subject to change if we ever inplement more than just y
         %intervals. For now lazily implemented
-        function interval = getLayerInterval(obj,rule)
+        function interval = getLayerInterval(obj,rule,fields)
             fractionLookup = @(iv,f)iv(1)+f*diff(iv);
             getIv = @(x)cat(2,obj.layerInfo(x).From,obj.layerInfo(x).To);
             
-            l = cat(1,str2double(rule.y_min_layer), str2double(rule.y_max_layer));
-            f = cat(1,str2double(rule.y_min_fraction), str2double(rule.y_max_fraction));
-            interval = cat(2,fractionLookup(getIv(l(1)),f(1)),fractionLookup(getIv(l(2)),f(2)));
+            iv = cellfun(@(str)getIv(str2double(rule.(cat(2,str,'_layer')))),fields,'UniformOutput',false);
+            f = cellfun(@(str)str2double(rule.(cat(2,str,'_fraction'))),fields,'UniformOutput',false);
+            interval = cellfun(fractionLookup,iv,f);
+            %l = cat(1,str2double(rule.y_min_layer), str2double(rule.y_max_layer));
+            %f = cat(1,str2double(rule.y_min_fraction), str2double(rule.y_max_fraction));
+            %interval = cat(2,fractionLookup(getIv(l(1)),f(1)),fractionLookup(getIv(l(2)),f(2)));
         end                      
         
         function [] = addRulesFromXml(obj,xml)
@@ -174,7 +181,11 @@ classdef XmlSpecifiedRuleCheck < handle
             nameValueLookup = @(x,str)ifElseExpansion(any(cellfun(@(s)strcmp(s,str),{x.Name})),{'',x(cellfun(@(s)strcmp(s,str),{x.Name})).Value});
             
             for i = 1:length(xml)
-                currMType = nameValueLookup(xml(i).Attributes,'mtype');
+                if(strcmp(xml(i).Name,'global_rule_set'))
+                    currMType = 'global';
+                else
+                    currMType = nameValueLookup(xml(i).Attributes,'mtype');
+                end
                 obj.ruleSets.(currMType).mtype = currMType;
                 %If we ever have other types of rules add that case here.
                 rules = xml(i).Children;
@@ -199,6 +210,24 @@ classdef XmlSpecifiedRuleCheck < handle
                 obj.layerBins{i} = (borders(1:end-1) + borders(2:end))./2;
             end
         end
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %      Specific functions for different rule types       %
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        function scores = checkRegionTarget(obj,instance,rule,bins)            
+            minMaxFcn = @(x,y)cat(2,max(x(1),y(1)),min(x(2),y(2)));
+            intervalAbs = obj.getLayerInterval(rule,{'y_min','y_max'}); 
+            intervalRel = [str2double(instance.y_min) str2double(instance.y_max)];
+            scores = cellfun(@(x)diff(minMaxFcn(intervalAbs,intervalRel+x)),num2cell(bins))./min(diff(intervalAbs),diff(intervalRel));
+            scores(scores<0)=0;
+            scores = scores.^2;
+        end
+        function scores = checkYBelow(obj,instance,rule,bins)            
+            limitAbs = obj.getLayerInterval(rule,{'y'});
+            limitRel = str2double(instance.y_max);
+            scores = 2.*(limitRel+bins<=limitAbs)-1;            
+        end
+        
     end
     
     
@@ -206,7 +235,7 @@ classdef XmlSpecifiedRuleCheck < handle
         %Implement transfer functions here
         function dat = upper_hard_limit(dat)
             dat(find(dat(2:end)<dat(1:end-1))+1)=0;
-        end
+        end        
     end
 end
 
