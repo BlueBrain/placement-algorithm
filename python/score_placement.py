ee348a08566a86e2d2c4bd0f886a3b0204fe0e37
Arseny V. Povolotsky <arseny.povolotsky@epfl.ch>
2017-10-10 11:07:40 +0200

    Placement rules by mtype (NSETM-281)
    Change-Id: I5f794c5ff3bc84db3dd59c8bf6957efaf8853a98

--- python/score_placement.py
+++ python/score_placement.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import copy
 import argparse
 import logging
 
@@ -10,8 +11,14 @@ import numpy as np
 import pandas as pd
 
 
-# Optional score to give to annotations with no optional rules
-BASE_OPTIONAL_SCORE = 0.1
+# Score to give to rules with missing annotations
+MISSING_RULE_SCORE = 0.1
+
+# Known annotations to ignore
+IGNORED_RULES = {
+    'ScaleBias'
+}
+
 
 L = logging.getLogger(__name__)
 
@@ -26,7 +33,8 @@ def y_absolute(y_rel, candidate):
 
 class YBelowRule(object):
     """ Check that 'y' does not exceed given limit. """
-    def __init__(self, y_rel, tolerance):
+    def __init__(self, _id, y_rel, tolerance):
+        self.id = _id
         self.y_rel = y_rel
         self.tolerance = tolerance
 
@@ -34,7 +42,7 @@ class YBelowRule(object):
     def from_attr(cls, attr):
         y_rel = attr['y_layer'], float(attr['y_fraction'])
         tolerance = float(attr.get('tolerance', 30.0))
-        return cls(y_rel=y_rel, tolerance=tolerance)
+        return cls(attr['id'], y_rel=y_rel, tolerance=tolerance)
 
     def __call__(self, candidate, annotation):
         """ score = 1.0 - clip((y_candidate - y_annotation) / tolerance, 0.0, 1.0). """
@@ -55,7 +63,8 @@ class YBelowRule(object):
 
 class YRangeOverlapRule(object):
     """ Check that '[y1; y2]' falls within given interval. """
-    def __init__(self, y_rel_min, y_rel_max):
+    def __init__(self, _id, y_rel_min, y_rel_max):
+        self.id = _id
         self.y_rel_min = y_rel_min
         self.y_rel_max = y_rel_max
 
@@ -63,7 +72,7 @@ class YRangeOverlapRule(object):
     def from_attr(cls, attr):
         y_rel_min = attr['y_min_layer'], float(attr['y_min_fraction'])
         y_rel_max = attr['y_max_layer'], float(attr['y_max_fraction'])
-        return cls(y_rel_min=y_rel_min, y_rel_max=y_rel_max)
+        return cls(attr['id'], y_rel_min=y_rel_min, y_rel_max=y_rel_max)
 
     def __call__(self, candidate, annotation):
         """ score = overlap(candidate, annotation) / max_possible_overlap(candidate, annotation). """
@@ -99,7 +108,7 @@ def aggregate_optional_scores(scores, p):
     if len(scores) > 0:
         return generalized_mean(1.0 + scores, p) - 1.0
     else:
-        return BASE_OPTIONAL_SCORE
+        return 1.0
 
 
 def aggregate_strict_scores(scores):
@@ -109,16 +118,16 @@ def aggregate_strict_scores(scores):
         return 1.0
 
 
-def score_candidate(candidate, annotation, rules, p=1.0):
+def score_candidate(candidate, morph_rules, p=1.0):
     log_tag = "(%s; %s)" % (candidate['id'], candidate['morph'])
     strict_scores, optional_scores = [], []
-    for item in annotation:
-        if item['rule'] not in rules:
-            L.warning("Ignoring rule %s", item['rule'])
-            continue
-        rule = rules[item['rule']]
-        score = rule(candidate=candidate, annotation=item)
-        L.debug("%s: score=%.3f (%s)", log_tag, score, item['rule'])
+
+    for rule, annotation in morph_rules:
+        if annotation is None:
+            score = MISSING_RULE_SCORE
+        else:
+            score = rule(candidate=candidate, annotation=annotation)
+        L.debug("%s: score=%.3f (%s)", log_tag, score, rule.id)
         if rule.strict:
             strict_scores.append(score)
         else:
@@ -130,29 +139,87 @@ def score_candidate(candidate, annotation, rules, p=1.0):
     return strict_score * optional_score
 
 
-def load_rules(filepath):
-    """ Parse XML with placement rules. """
+def parse_rule_set(group):
     result = {}
-    etree = lxml.etree.parse(filepath)
-    for elem in etree.findall('*/rule'):
+    for elem in group.findall('rule'):
         attr = elem.attrib
         rule_id = attr['id']
+        if rule_id in result:
+            raise RuntimeError("Duplicate rule '%s'", rule_id)
         rule_type = attr['type']
         if rule_type in DISPATCH_RULE:
             result[rule_id] = DISPATCH_RULE[rule_type].from_attr(attr)
         else:
-            L.debug("Unknown placement rule type: %s", rule_type)
+            L.warning("Unknown placement rule type: %s, skipping", rule_type)
     return result
 
 
-def load_annotation(filepath):
-    """ Parse XML with morphology annotations. """
+def load_rules(filepath):
+    """ Parse XML with placement rules. """
     etree = lxml.etree.parse(filepath)
-    return [elem.attrib for elem in etree.findall('placement')]
+
+    elems = etree.findall('/global_rule_set')
+    if len(elems) < 1:
+        common_rules = {}
+    elif len(elems) == 1:
+        common_rules = parse_rule_set(elems[0])
+    elif len(elems) > 1:
+        raise RuntimeError("Duplicate <global_rule_set>")
+
+    mtype_rules = {}
+    for elem in etree.findall('/mtype_rule_set'):
+        mtype = elem.attrib['mtype']
+        if mtype in mtype_rules:
+            raise RuntimeError("Duplicate <mtype_rule_set> for mtype '%s'", mtype)
+        mtype_rules[mtype] = parse_rule_set(elem)
+
+    return common_rules, mtype_rules
+
+
+def load_annotation(annotation_dir, morphology):
+    """ Parse XML with morphology annotations. """
+    filepath = os.path.join(annotation_dir, morphology + ".xml")
+    etree = lxml.etree.parse(filepath)
+    return [elem.attrib for elem in etree.findall('/placement')]
+
+
+class PlacementRules(object):
+    def __init__(self, filepath):
+        self.common_rules, self.mtype_rules = load_rules(filepath)
+
+    def bind(self, annotation, mtype):
+        """ Bind annotation to placement rules. """
+        common_rules = copy.copy(self.common_rules)
+        mtype_rules = copy.copy(self.mtype_rules.get(mtype, {}))
+
+        result = []
+        for item in annotation:
+            params = copy.copy(item)
+            rule_id = params.pop('rule')
+            if rule_id in mtype_rules:
+                rule = mtype_rules.pop(rule_id)
+            elif rule_id in common_rules:
+                rule = common_rules.pop(rule_id)
+            elif rule_id in IGNORED_RULES:
+                continue
+            else:
+                L.warning("Unknown rule: '%s', skipping", rule_id)
+                continue
+            result.append((rule, params))
+
+        for rule_id, rule in common_rules.iteritems():
+            L.warning("Missing 'rule' annotation: '%s'", rule_id)
+            result.append((rule, None))
+
+        for rule_id, rule in mtype_rules.iteritems():
+            L.warning("Missing rule annotation: '%s'", rule_id)
+            result.append((rule, None))
+
+        return result
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Score placement candidates.")
+    parser = argparse.ArgumentParser(description="Score placement candidates")
     parser.add_argument(
         "-r", "--rules",
         help="Path to placement rules",
@@ -194,11 +261,11 @@ if __name__ == '__main__':
         2: logging.DEBUG,
     }[args.verbose])
 
-    rules = load_rules(args.rules)
+    rules = PlacementRules(args.rules)
 
     layers = args.layers.split(",")
 
-    base_columns = ['morph', 'id', 'y']
+    base_columns = ['mtype', 'morph', 'id', 'y']
     if args.profile:
         profile = np.array(args.profile.split(","), dtype=float)
         profile/= np.sum(profile)
@@ -215,12 +282,12 @@ if __name__ == '__main__':
         candidates = pd.read_csv(sys.stdin, sep=r'\s+', names=base_columns + layer_columns)
 
     for _, candidate in candidates.iterrows():
-        annotation_path = os.path.join(args.annotations, candidate.morph + ".xml")
         try:
-            annotation = load_annotation(annotation_path)
+            annotation = load_annotation(args.annotations, candidate.morph)
         except IOError:
             L.warning("No annotation found for '%s', skipping", candidate.morph)
             continue
-        score = score_candidate(candidate, annotation, rules, p=args.p_order)
+        morph_rules = rules.bind(annotation, candidate.mtype)
+        score = score_candidate(candidate, morph_rules, p=args.p_order)
         print candidate.morph, candidate.id, "%.3f" % score
 
--- python/tests/test_score_placement.py
+++ python/tests/test_score_placement.py
@@ -19,7 +19,7 @@ def test_y_absolute():
 
 
 def test_y_below_rule():
-    rule = test_module.YBelowRule(y_rel=('L1', 0.5), tolerance=10.0)
+    rule = test_module.YBelowRule('test', y_rel=('L1', 0.5), tolerance=10.0)
     candidate = { 'y': 10, 'L1_0': 20, 'L1_1': 30 }
     nt.assert_equal(rule(candidate, {'y_max': 16}), 0.9)
     nt.assert_equal(rule(candidate, {'y_max':  0}), 1.0)
@@ -27,7 +27,7 @@ def test_y_below_rule():
 
 
 def test_y_range_overlap():
-    rule = test_module.YRangeOverlapRule(y_rel_min=('L1', 0.0), y_rel_max=('L1', 0.5))
+    rule = test_module.YRangeOverlapRule('test', y_rel_min=('L1', 0.0), y_rel_max=('L1', 0.5))
     candidate = { 'y': 20, 'L1_0': 20, 'L1_1': 30 }
     nt.assert_equal(rule(candidate, {'y_min': -4, 'y_max': -2}), 0.0)
     nt.assert_equal(rule(candidate, {'y_min': -2, 'y_max':  2}), 0.5)
@@ -52,7 +52,7 @@ def test_generalized_mean():
 def test_aggregate_optional_scores():
     nt.assert_equal(
         test_module.aggregate_optional_scores([], 1.0),
-        0.1
+        1.0
     )
     nt.assert_almost_equal(
         test_module.aggregate_optional_scores([0.0, 0.5, 1.0], p=1.0),
@@ -85,11 +85,11 @@ def test_load_annotation():
         {'rule': 'L1_hard_limit', 'y_min': '-223.907318115', 'y_max': '33.7012710571' },
         {'rule': 'L1_axon_hard_limit', 'y_min': '-217.9246521', 'y_max': '38.8493537903' },
     ]
-    result = test_module.load_annotation(os.path.join(TEST_DATA_DIR, "C060106F.xml"))
+    result = test_module.load_annotation(TEST_DATA_DIR, "C060106F")
     nt.assert_equal(expected, result)
 
 
 def test_load_rules():
-    result = test_module.load_rules(os.path.join(TEST_DATA_DIR, "rules.xml"))
-    nt.assert_is_instance(result['L1_hard_limit'], test_module.YBelowRule)
-    nt.assert_is_instance(result['L1_HAC, axon, Layer_1'], test_module.YRangeOverlapRule)
+    common_rules, mtype_rules = test_module.load_rules(os.path.join(TEST_DATA_DIR, "rules.xml"))
+    nt.assert_is_instance(common_rules['L1_hard_limit'], test_module.YBelowRule)
+    nt.assert_is_instance(mtype_rules['L1_HAC']['L1_HAC, axon, Layer_1'], test_module.YRangeOverlapRule)
