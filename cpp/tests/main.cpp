a4463cd7d1dda044c9def39679dce993556dfffb
Arseny V. Povolotsky <arseny.povolotsky@epfl.ch>
2017-12-18 14:11:56 +0100

    Added some tests
    Change-Id: I28e1703880c87526b91b576f262f69077f5809c7

---
+++ cpp/tests/main.cpp
@@ -0,0 +1,14 @@
+#define CATCH_CONFIG_MAIN
+#include "../contrib/catch.hpp"
+
+
+TEST_CASE( "Parse rules XML", "[rules]" )
+{
+    // TODO
+}
+
+
+TEST_CASE( "Parse annotation XML", "[annotation]" )
+{
+    // TODO
+}
